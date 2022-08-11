44ce8bc477d6c9ef0aecfbb54a1f50a902bff9b7
vwkd <33468089+vwkd@users.noreply.github.com>
2022-08-11 07:53:32 +0000

    scripts: export_csv with numbered fields

--- scripts/export_csv.sh
+++ scripts/export_csv.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-# replaces first space with unique separator `@`
+# export as CSV
 # import CSV in spreadsheet with unique separator `@`
+# beware: relies on error correction of filling in missing separators in lines without separator!
 
-sed -r 's/^([^ ]+) /\1@/g' vz/vz.txt > exp.csv
+# replaces first space, digit bigger than 1 and dot with unique separator `@`
+# deletes 1 and dot
+
+sed -r 's/^([^ ]+) /\1@/g; s/ [[:digit:]]+\. /@/g; s/1\. //g' vz/vz.txt > exp.csv
