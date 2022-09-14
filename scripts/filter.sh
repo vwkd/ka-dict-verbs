b44dce6fa4b1c4dcd8c31e96189b9ff829e275c5
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-14 21:40:33 +0400

    scripts: clean up filter

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # filter out lines containing symbols
-# "/", "(", ")", "-", "u. ä.", "od.", "usw.", "bzw.", '"', "Pkt."
+# "(", ")", "-", "u. ä.", "usw.", "bzw.", '"', "Pkt."
 
-sed -r '/\/|\(|\)|\-|u\.|od\.|usw\.|bzw\.|\"|Pkt\./d' vz/vz.txt > vz_filtered.txt
+sed -r '/\(|\)|\-|u\.|usw\.|bzw\.|\"|Pkt\./d' vz/vz.txt > vz_filtered.txt
